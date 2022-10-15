02e83ea7af1759e018868c22d92751c2e5bfc283
shahriarAb <sm.abeed007@gmail.com>
2022-10-15 16:38:48 +0600

    tailwind and react router installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Canadian Blood Service.</h1>
+      <h1 className="text-4xl text-red-700 font-bold">
+        Welcome to Canadian Blood Service.
+      </h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
