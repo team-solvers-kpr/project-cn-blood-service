6ceeba4b10d8e0fed6cd5a20014c4b73fb497fe4
hparvez905 <hparvez905@gmail.com>
2022-10-23 13:34:49 +0600

    HomeCard removed

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
-import HomeCard from "./HomeCard/HomeCard";
 import HomeSection from "./HomeSection";
 
 const Home = () => {
   return (
     <div>
       <h2>This is home</h2>
-      <HomeCard></HomeCard>
       <HomeSection></HomeSection>
     </div>
   );
