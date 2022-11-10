a992430eafd1c0233de384fcf0db69dd6908253e
shahriarAb <sm.abeed007@gmail.com>
2022-11-10 13:11:43 +0600

    sign in page completed with google sign in

--- src/components/Pages/Login/Signin/Signin.js
+++ src/components/Pages/Login/Signin/Signin.js
@@ -1,9 +1,66 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Signup from "../Signup/Signup";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../../../firebase.init";
 
 const Signin = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  console.log(user);
+
   return (
-    <div className="my-20">
-      <h2>This is sign in section.</h2>
+    <div className="bg-gray-100">
+      <h2 className="text-start text-xl font-semibold text-gray-500 ml-10 pt-5 pb-3">
+        Sign in or create an account
+      </h2>
+      <hr className="py-6" />
+      <div className="grid grid-cols-2 gap-8">
+        <div>
+          <h2 className="text-2xl font-semibold">Sign in</h2>
+          <div className="text-start bg-white my-8 ml-8 rounded-md">
+            <div className="px-5 py-10">
+              <h3 className="text-lg">
+                Username <span className="text-red-500">*</span>
+              </h3>
+              <input
+                type="text"
+                placeholder="Username or email"
+                className="px-4 text-lg border-2 border-gray-200 w-full hover:shadow-lg focus:shadow-lg rounded-md my-2"
+              />
+              <h3 className="text-lg mt-4">
+                Password <span className="text-red-500">*</span>
+              </h3>
+              <input
+                type="password"
+                placeholder="Password"
+                className="px-4 text-lg border-2 border-gray-200 w-full hover:shadow-lg focus:shadow-lg rounded-md my-2"
+              />
+              <button className="my-10 py-3 px-6 bg-red-600 rounded-full text-white text-lg font-semibold">
+                Log in
+              </button>
+              <div>
+                <Link className="text-red-600 underline hover:no-underline text-lg">
+                  Forgot username
+                </Link>
+                <br />
+                <Link className="text-red-600 underline hover:no-underline text-lg">
+                  Forgot password
+                </Link>
+              </div>
+              <div className="py-8 divider">OR</div>
+              <button
+                onClick={() => signInWithGoogle()}
+                className="btn btn-outline btn-warning w-full"
+              >
+                Continue with Google
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <Signup></Signup>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Pages/Login/Signup/Signup.js
+++ src/components/Pages/Login/Signup/Signup.js
@@ -3,7 +3,96 @@ import React from "react";
 const Signup = () => {
   return (
     <div>
-      <h2>This is signup section</h2>
+      <h2 className="text-2xl font-semibold">Create an account</h2>
+      <div className="text-start bg-white my-8 mr-8 rounded-md">
+        <div className="px-5 py-10">
+          <h2 className="text-xl">You are just a step away!</h2>
+          <h2 className="my-4 text-lg">
+            Complete the form below to create an account. Once you've completed
+            the required information, proceed to the next step.
+          </h2>
+          <h2 className="text-lg my-2">
+            Thank you for being part of Canada's lifeline.
+          </h2>
+          <h3 className="text-lg mt-6">
+            Email address (this will be your username){" "}
+            <span className="text-red-500">*</span>
+          </h3>
+          <input
+            type="text"
+            className="px-4 text-lg border-2 border-gray-200 w-full hover:shadow-lg focus:shadow-lg rounded-md my-2"
+            required
+          />
+          <h3 className="text-lg mt-4">
+            Confirm Email address <span className="text-red-500">*</span>
+          </h3>
+          <input
+            type="text"
+            className="px-4 text-lg border-2 border-gray-200 w-full hover:shadow-lg focus:shadow-lg rounded-md my-2"
+          />
+          <h3 className="text-lg mt-4">
+            First name <span className="text-red-500">*</span>
+          </h3>
+          <input
+            type="text"
+            className="px-4 text-lg border-2 border-gray-200 w-full hover:shadow-lg focus:shadow-lg rounded-md my-2"
+          />
+          <h3 className="text-lg mt-4">
+            Last name <span className="text-red-500">*</span>
+          </h3>
+          <input
+            type="text"
+            className="px-4 text-lg border-2 border-gray-200 w-full hover:shadow-lg focus:shadow-lg rounded-md my-2"
+          />
+          <h3 className="text-lg mt-4">Donor number</h3>
+          <input
+            type="text"
+            placeholder="Enter your donor number (if you have one)"
+            className="px-4 text-lg border-2 border-gray-200 w-full hover:shadow-lg focus:shadow-lg rounded-md my-2"
+          />
+          <h3 className="text-lg mt-4">
+            Date of birth <span className="text-red-500">*</span>
+          </h3>
+          <input
+            type="date"
+            name="date of birth"
+            className="px-4 text-lg border-2 border-gray-200 w-1/4 hover:shadow-lg focus:shadow-lg rounded-md my-2"
+          />
+          <h3 className="text-lg mt-4">
+            Postal Code <span className="text-red-500">*</span>
+          </h3>
+          <input
+            type="text"
+            className="px-4 text-lg border-2 border-gray-200 w-full hover:shadow-lg focus:shadow-lg rounded-md my-2"
+          />
+          <h3 className="text-lg mt-4">
+            Phone number<span className="text-red-500">*</span>
+          </h3>
+          <input
+            type="tel"
+            className="px-4 text-lg border-2 border-gray-200 w-full hover:shadow-lg focus:shadow-lg rounded-md my-2"
+          />
+          <h3 className="text-lg mt-4">
+            Password <span className="text-red-500">*</span>
+          </h3>
+          <input
+            type="password"
+            placeholder="Password"
+            className="px-4 text-lg border-2 border-gray-200 w-full hover:shadow-lg focus:shadow-lg rounded-md my-2"
+          />
+          <h3 className="text-lg mt-4">
+            Confirm password <span className="text-red-500">*</span>
+          </h3>
+          <input
+            type="password"
+            placeholder="Password"
+            className="px-4 text-lg border-2 border-gray-200 w-full hover:shadow-lg focus:shadow-lg rounded-md my-2"
+          />
+          <button className="my-10 py-3 px-6 bg-red-600 rounded-full text-white text-lg font-semibold">
+            Sign up
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
