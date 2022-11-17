76ca10e7cdb237f6c477e0b7f553f4109b0ff22f
unknown <xahin.mahir@gmail.com>
2022-11-17 19:39:05 +0600

    shop category done

--- src/components/Pages/Shop/Shop/Shop.js
+++ src/components/Pages/Shop/Shop/Shop.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ShopAd from '../ShopAd/ShopAd';
 import ShopBanner from '../ShopBanner/ShopBanner';
+import ShopCategory from '../ShopCategory/ShopCategory';
 
 const Shop = () => {
     return (
         <div>
             <ShopBanner />
             <ShopAd />
+            <ShopCategory />
         </div>
     );
 };
