04741988cbf979310c0178a94bcc4b7c5d78e342
shafiuljony <shafiuljony12@gmail.com>
2022-11-18 01:28:18 +0600

    modal

--- src/components/Modal/VideoModal.jsx
+++ src/components/Modal/VideoModal.jsx
@@ -1,5 +1,6 @@
 import { Dialog, Transition } from '@headlessui/react'
 import React, { Fragment, useState } from 'react'
+import { CgClose } from 'react-icons/cg'
 
 const VideoModal = () => {
     let [isOpen, setIsOpen] = useState(true)
@@ -20,7 +21,7 @@ const VideoModal = () => {
             onClick={openModal}
             className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
           >
-            Open dialog
+            <img src="{}" alt="" />
           </button>
         </div>
   
@@ -69,7 +70,7 @@ const VideoModal = () => {
                         className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                         onClick={closeModal}
                       >
-                        Got it, thanks!
+                        <CgClose />
                       </button>
                     </div>
                   </Dialog.Panel>
