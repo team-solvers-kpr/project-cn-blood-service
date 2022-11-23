06d0ade5b83a8d19dff0c9e132bfb5a334476110
hparvez905 <hparvez905@gmail.com>
2022-11-23 14:14:09 +0600

    responsive update for about page

--- src/components/Pages/AboutUs/UpperPart.js
+++ src/components/Pages/AboutUs/UpperPart.js
@@ -5,18 +5,18 @@ import bgImage from '../../assets/img.jpg';
 
 const UpperPart = () => {
   return (
-    <div className="lg:flex">
-      <div className=" relative">
-            <img src={bgImage} alt="" className="lg:py-32 lg:pl-60 "/>
+    <div className="lg:flex mb-10">
+      <div className=" lg:relative hidden lg:block">
+            <img src={bgImage} alt="" className="lg:py-32 lg:pl-60"/>
         </div>
-        <div className=" absolute p-10 lg:mx-32 lg:mt-32">
+        <div className=" lg:absolute lg:p-10 p-4 lg:mx-32 lg:mt-32">
         
-            <h1 className="text-6xl font-bold text-justify mb-4 text-gray-700">About Canadian Blood <br></br> Services</h1>
+            <h1 className="lg:text-6xl text-5xl font-bold text-justify mb-4 text-gray-700">About Canadian Blood <br></br> Services</h1>
             <h4 className="text-lg font-bold text-justify mb-4 text-gray-700">We are Canada's Biological Lifeline.</h4>
-            <p className="text-justify text-gray-600 text-lg w-1/2">We are the connection between donors and patients, healthcare professionals and medical researchers. We are nationally responsible for a secure system of life essentials for transfusion and transplantation that’s reliable, accessible and sustainable. Every day we work diligently to help save lives, restore health and earn the nation’s trust.</p>
-            <div className="border-b-4 my-2 border-red-600 rounded-md w-32"></div>
+            <p className="text-base text-justify text-gray-600 text-lg lg:w-1/2">We are the connection between donors and patients, healthcare professionals and medical researchers. We are nationally responsible for a secure system of life essentials for transfusion and transplantation that’s reliable, accessible and sustainable. Every day we work diligently to help save lives, restore health and earn the nation’s trust.</p>
+            <div className="border-b-4 my-2 border-red-600 rounded-md w-32 mt-10 "></div>
           <div>
-          <div className="flex">
+          <div className="flex mt-4">
            <div className="mt-4"> <AiOutlineArrowRight className="text-emerald-600 text-lg"></AiOutlineArrowRight></div>
            <div className=" "> <p className="hover:text-red-800 cursor-pointer text-red-600 text-justify text-lg  mt-2"><a href="https://www.blood.ca/en/about-us/publications-and-reports" target='_blank'  rel="noopener noreferrer">Who we are </a> </p></div>
             
