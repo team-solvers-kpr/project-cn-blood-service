cd77517250f8ceb83a4d71d0001c8e2cac176d4b
hparvez905 <hparvez905@gmail.com>
2022-11-26 18:58:10 +0600

    Research page Subscribe Part update

--- src/components/Pages/Research/Subscribe.js
+++ src/components/Pages/Research/Subscribe.js
@@ -1,3 +1,4 @@
+import { Input } from "postcss";
 import React from "react";
 
 
@@ -24,10 +25,8 @@ const Subscribe = () => {
  </div>
         {/* right side  */}
  <div className="w-1/2  px-28 py-12">
-        <h1 className="hover:text-red-800 text-3xl font-bold cursor-pointer hover:no-underline text-red-700 text-justify"><a href="https://www.blood.ca/en/research/our-research-activities"> Our Research Stories</a></h1>
-        <p className="text-justify mt-2 text-base text-gray-600">
-        Every project tells a story. Discover our research stories on RED, our research, education, and discovery blog. Read an overview of research findings and impacts in our Centre for Innovation progress reports and in our accessible research summaries.
-    </p>
+        <h1 className=" text-sm text-gray-500 text-justify">Email Address <span className="text-red-600">*</span></h1>
+       
     <div className="flex mt-6">
           
           <div className=" "> <p className="hover:text-red-800 cursor-pointer text-red-700 text-justify text-base font-semibold mt-1"><a href="https://www.blood.ca/en/research/our-research-stories" target='_blank'  rel="noopener noreferrer">Learn more about Our Research Stories </a> </p></div>
