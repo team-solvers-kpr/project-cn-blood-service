29269a3a7c8f2ff5d69f44fac1c9d652561abf44
shafiuljony <shafiuljony12@gmail.com>
2022-12-03 21:30:41 +0600

    search bar

--- src/components/Pages/Shared/Navbar/Navbar.jsx
+++ src/components/Pages/Shared/Navbar/Navbar.jsx
@@ -117,7 +117,9 @@ const Navbar = () => {
                                 </div>
                             </div>
                             <div className='topsearchbar '>
-                                <SearchBar width='w-12'  />
+                                <button className="btn btn-ghost btn-circle bg-red-600 text-white">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 " fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                                </button>
                             </div>
                         </div>
                     </nav>
