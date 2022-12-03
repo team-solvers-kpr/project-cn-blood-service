9dcd2c7615e40fae90803d8b5a232a15e5c3542c
team-solvers-kpr <113278332+team-solvers-kpr@users.noreply.github.com>
2022-12-03 14:17:37 +0600

    conflict fixed from other branches

--- src/App.js
+++ src/App.js
@@ -1,62 +1,55 @@
 import { useLayoutEffect } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
-import Signin from "./components/pages/Login/Signin/Signin";
-import AllProducts from "./components/pages/Shop/AllProducts/AllProducts";
-import Shop from "./components/pages/Shop/Shop/Shop";
-import Home from "./components/pages/Home/Home/Home";
-import Navbar from "./components/pages/Shared/Navbar/Navbar";
-import HospitalServices from "./components/pages/Hospital-services/HospitalServices";
-import AboutUs from "./components/pages/AboutUs/AboutUs";
-import Notfound from "./components/pages/Notfound/Notfound";
-import Footer from "./components/pages/Shared/Footer/Footer";
-import Research from "./components/pages/Research/Research";
-import Careers from "./components/pages/Careers/Careers";
-
+import Signin from "./components/Pages/Login/Signin/Signin";
+import AllProducts from "./components/Pages/Shop/AllProducts/AllProducts";
+import Shop from "./components/Pages/Shop/Shop/Shop";
+import Home from "./components/Pages/Home/Home/Home";
+import Navbar from "./components/Pages/Shared/Navbar/Navbar";
+import HospitalServices from "./components/Pages/Hospital-services/HospitalServices";
+import AboutUs from "./components/Pages/AboutUs/AboutUs";
+import Notfound from "./components/Pages/Notfound/Notfound";
+import Footer from "./components/Pages/Shared/Footer/Footer";
+import Research from "./components/Pages/Research/Research";
+import Careers from "./components/Pages/Careers/Careers";
 
 function App() {
-    const Wrapper = ({ children }) => {
-        const location = useLocation();
+  const Wrapper = ({ children }) => {
+    const location = useLocation();
 
-        useLayoutEffect(() => {
-            document.documentElement.scrollTo(0, 0);
-        }, [location.pathname]);
-        return children;
-    };
+    useLayoutEffect(() => {
+      document.documentElement.scrollTo(0, 0);
+    }, [location.pathname]);
+    return children;
+  };
 
-    return (
-        <div className="App">
-            <Wrapper>
-                <Navbar></Navbar>
-                <Routes>
-                    <Route path="/" element={<Home />}></Route>
-                    <Route path="/home" element={<Home />}></Route>
-                    <Route
-                        path="/hospital-services"
-                        element={<HospitalServices />}
-                    ></Route>
-                    <Route path="/signin" element={<Signin />}></Route>
-                    <Route path="/shop" element={<Shop />}></Route>
-                    <Route
-                        path="/allProducts"
-                        element={<AllProducts />}
-                    ></Route>
-                    <Route
-                        path="/hospital-services"
-                        element={<HospitalServices />}
-                    ></Route>
-                    <Route
-                        path="/careers"
-                        element={<Careers />}
-                    ></Route>
-                    <Route path="/about-Us" element={<AboutUs />}></Route>
-                    <Route path="/research" element={<Research />}></Route>
-                    <Route path="*" element={<Notfound />}></Route>
-                </Routes>
-                <Footer></Footer>
-            </Wrapper>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Wrapper>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route
+            path="/hospital-services"
+            element={<HospitalServices />}
+          ></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/shop" element={<Shop />}></Route>
+          <Route path="/allProducts" element={<AllProducts />}></Route>
+          <Route
+            path="/hospital-services"
+            element={<HospitalServices />}
+          ></Route>
+          <Route path="/careers" element={<Careers />}></Route>
+          <Route path="/about-Us" element={<AboutUs />}></Route>
+          <Route path="/research" element={<Research />}></Route>
+          <Route path="*" element={<Notfound />}></Route>
+        </Routes>
+        <Footer></Footer>
+      </Wrapper>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Pages/Research/Subscribe.js
+++ src/components/Pages/Research/Subscribe.js
@@ -1,42 +1,50 @@
-import { Input } from "postcss";
 import React from "react";
 import Email from "./ValidateEmail/Email";
 
-
-
-
-
 const Subscribe = () => {
   return (
- <div className="bg-gray-100">
-    <div className="flex justify-between">
+    <div className="bg-gray-100">
+      <div className="flex justify-between">
         {/* left side  */}
-    <div className=" w-1/2 px-28 py-12">
-    <h1 className="text-gray-600 text-4xl font-bold text-justify">Subscribe</h1>
-    <p className="text-justify mt-4 text-base text-gray-600">
-    The latest news, publications and events from Canadian Blood Services’ Medical Affairs and Innovation research and education network.
-    </p>
-    <div className="flex mt-6">
-          
-           <div className=" "> <p className="hover:text-red-800 cursor-pointer text-red-700 text-justify text-base font-semibold underline mt-1"><a href="https://us12.campaign-archive.com/home/?u=8fcd40bcdd74ee6eede38f01c&id=cc7f69014e" target='_blank'  rel="noopener noreferrer">View Past Issues </a> </p></div>
-           
-            
-       
-    </div>
- </div>
-        {/* right side  */}
- <div className=" w-1/2  px-8 mt-8 py-9">
-       <p className="text-xs text-gray-600 flex justify-end mr-32"><span className="text-red-400">* </span>indicates required</p>
-        <div className="flex">
-        <h1 className=" text-gray-700 text-justify text-sm">Email Address </h1>
-        <p className="text-red-400 text-lg ml-1">*</p>
+        <div className=" w-1/2 px-28 py-12">
+          <h1 className="text-gray-600 text-4xl font-bold text-justify">
+            Subscribe
+          </h1>
+          <p className="text-justify mt-4 text-base text-gray-600">
+            The latest news, publications and events from Canadian Blood
+            Services’ Medical Affairs and Innovation research and education
+            network.
+          </p>
+          <div className="flex mt-6">
+            <div className=" ">
+              {" "}
+              <p className="hover:text-red-800 cursor-pointer text-red-700 text-justify text-base font-semibold underline mt-1">
+                <a
+                  href="https://us12.campaign-archive.com/home/?u=8fcd40bcdd74ee6eede38f01c&id=cc7f69014e"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View Past Issues{" "}
+                </a>{" "}
+              </p>
+            </div>
+          </div>
         </div>
-       <Email></Email>
-   
-   
- </div>
+        {/* right side  */}
+        <div className=" w-1/2  px-8 mt-8 py-9">
+          <p className="text-xs text-gray-600 flex justify-end mr-32">
+            <span className="text-red-400">* </span>indicates required
+          </p>
+          <div className="flex">
+            <h1 className=" text-gray-700 text-justify text-sm">
+              Email Address{" "}
+            </h1>
+            <p className="text-red-400 text-lg ml-1">*</p>
+          </div>
+          <Email></Email>
+        </div>
+      </div>
     </div>
- </div>
   );
 };
 
