e79cae3422e2e0eb7b062730195ccd474891dfcb
hparvez905 <hparvez905@gmail.com>
2022-12-11 03:00:02 +0600

    Research responsive done

--- src/components/Pages/Research/MiddlePart/MiddlePartTwo.js
+++ src/components/Pages/Research/MiddlePart/MiddlePartTwo.js
@@ -28,11 +28,11 @@ const MiddlePartOne = () => {
  </div>
  {/* right side  */}
     <div className="bg-gray-100 lg:w-1/2 py-12 px-28">
-    <h1 className=" lg:pl-28 hover:text-red-800 text-3xl font-bold cursor-pointer hover:no-underline text-red-700 text-justify"><a href="https://www.blood.ca/en/research/our-research-activities"> Products and Services for Researchers</a></h1>
-    <p className="lg:px-28 text-justify mt-10 text-base text-gray-600">
+    <h1 className="  hover:text-red-800 text-3xl font-bold cursor-pointer hover:no-underline text-red-700 text-justify"><a href="https://www.blood.ca/en/research/our-research-activities"> Products and Services for Researchers</a></h1>
+    <p className=" text-justify mt-10 text-base text-gray-600">
     Discover more about products and services offered by Canadian Blood Services:
     </p>
-    <div className="flex mt-6 lg:px-28">
+    <div className="flex mt-6 ">
           
            <div className=" "> <p className="hover:text-red-800 cursor-pointer text-red-700 text-justify text-base font-semibold mt-1"><a href="https://www.blood.ca/en/research/products-and-services-researchers" target='_blank'  rel="noopener noreferrer">Learn more about Products and Services for Researchers </a> </p></div>
            <div className="mt-2 ml-4"> <FaArrowRight className="text-cyan-400  text-sm "></FaArrowRight></div>
