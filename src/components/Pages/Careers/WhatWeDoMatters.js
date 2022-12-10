df7c1d4648b5495c019e72b67f2fcd363531d123
hparvez905 <hparvez905@gmail.com>
2022-12-11 01:50:27 +0600

    career responsive done

--- src/components/Pages/Careers/WhatWeDoMatters.js
+++ src/components/Pages/Careers/WhatWeDoMatters.js
@@ -5,14 +5,14 @@ import bgImage from '../../assets/img.jpg';
 
 const WhatWeDoMatters = () => {
   return (
-    <div className="lg:flex">
-      <div className=" lg:relative hidden lg:block lg:ml-48 ">
-            <img src={bgImage} alt="" className="lg:ml-96 lg:h-96"/>
+    <div className="md:flex ">
+      <div className=" md:relative hidden md:block md:ml-48 ">
+            <img src={bgImage} alt="" className=" 2xl:ml-96 2xl:h-96"/>
         </div>
-        <div className=" lg:absolute  p-4 lg:mx-32 lg:mt-8">
+        <div className=" md:absolute mt-2 p-1 lg:p-4 2xl:mx-32 lg:mt-8">
         
             <h1 className="lg:text-6xl text-5xl font-bold text-justify mb-4 text-stone-600">What we do matters</h1>
-           <p className="text-stone-600">Somewhere in Canada there’s a life you can save. Join <strong>Canada’s Lifeline</strong> today.</p>
+           <p className="text-stone-600 text-justify">Somewhere in Canada there’s a life you can save. Join <strong>Canada’s Lifeline</strong> today.</p>
             <div className="">
             <div className='flex mt-8' >
             <p className='text-red-600 text-3xl'><BsFillArrowRightCircleFill className='bg-white rounded-full border-2 border-red-600'></BsFillArrowRightCircleFill></p>
